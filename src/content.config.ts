ed8d460e12b2e3fb567b9cb30958384b4447a951
vishalicious213 <vishalicious213@gmail.com>
2025-05-12 09:47:55 -0700

    added /pages/posts/[...slug].astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,13 +7,13 @@ const blog = defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
     schema: z.object({
         title: z.string(),
-        pubDate: z.date(),
-        description: z.string(),
         author: z.string(),
+        description: z.string(),
         image: z.object({
             url: z.string(),
             alt: z.string()
         }),
+        pubDate: z.date(),
         tags: z.array(z.string())
     })
 })
